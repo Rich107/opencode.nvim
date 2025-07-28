9c2b7f1e746ca5b823fcc67443a0752f7c76f19b
Nick van Dyke <nickjvandyke15@gmail.com>
2025-07-28 12:48:02 -0600

    feat: show all completions when no word started

--- lua/opencode/cmp.lua
+++ lua/opencode/cmp.lua
@@ -11,21 +11,18 @@ local config = require("opencode.config")
 return function(ArgLead, CmdLine, CursorPos)
   -- Find the start and end of the latest word in CmdLine
   local start_idx, end_idx = CmdLine:find("([^%s]+)$")
-  if not start_idx then
-    return {}
-  end
-
-  local latest_word = CmdLine:sub(start_idx, end_idx)
+  local latest_word = start_idx and CmdLine:sub(start_idx, end_idx) or nil
 
   local items = {}
   for placeholder, _ in pairs(config.options.context) do
-    if placeholder:find(latest_word, 1, true) == 1 then
-      -- Replace the latest word in CmdLine with the placeholder.
-      -- Normally for vim's customlist completion we'd just return the placeholder/completion item,
-      -- but snacks.input seems to pass the entire command line as ArgLead and thus also replaces the
-      -- entire command line with the completion item.
-      local new_cmd = CmdLine:sub(1, start_idx - 1) .. placeholder .. CmdLine:sub(end_idx + 1)
+    if not latest_word then
+      local new_cmd = CmdLine .. placeholder
       table.insert(items, new_cmd)
+    else
+      if placeholder:find(latest_word, 1, true) == 1 then
+        local new_cmd = CmdLine:sub(1, start_idx - 1) .. placeholder .. CmdLine:sub(end_idx + 1)
+        table.insert(items, new_cmd)
+      end
     end
   end
   return items
