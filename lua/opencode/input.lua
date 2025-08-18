461609941820f3080009fbaaa26861cb4a075ae6
Nick van Dyke <nickjvandyke15@gmail.com>
2025-08-18 13:44:56 -0600

    docs: note

--- lua/opencode/input.lua
+++ lua/opencode/input.lua
@@ -24,6 +24,7 @@ local function highlight(input)
     end
   end
 
+  -- Must occur in-order or neovim will error
   table.sort(hls, function(a, b)
     return a[1] < b[1] or (a[1] == b[1] and a[2] < b[2])
   end)
